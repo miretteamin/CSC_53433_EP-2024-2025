5dba7f2dbbd676492a0bf14b0cd73f5b04c018bd
Mirette Moawad <miretteamin@gmail.com>
2024-12-07 14:13:29 +0100

    adding brushes files

---
+++ Assets/02 - Scripts/01 - Terrain Brushes/IncrementalBrush.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncrementalBrush : TerrainBrush {
+
+    public float delta = 1;
+
+    
+
+    public override void draw(int x, int z) {
+        for (int zi = -radius; zi <= radius; zi++) {
+            for (int xi = -radius; xi <= radius; xi++) {
+
+                Debug.Log("terrain.get(0, 0)");
+
+                float height = terrain.get(x + xi, z + zi) + delta;
+                terrain.set(x + xi, z + zi, height);
+            }
+        }
+    }
+}
